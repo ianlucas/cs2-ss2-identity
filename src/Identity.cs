03b538549f5a6afbddc070e1be588b4099caf52a
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-05-15 08:05:01 -0300

    Update plugin description (#14)

--- src/Identity.cs
+++ src/Identity.cs
@@ -14,7 +14,7 @@ namespace Identity;
     Version = "1.0.0",
     Name = "Identity",
     Author = "Ian Lucas",
-    Description = "Authenticates external users."
+    Description = "A SwiftlyS2 plugin for authenticating users"
 )]
 public partial class Identity(ISwiftlyCore core) : BasePlugin(core)
 {
